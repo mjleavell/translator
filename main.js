0cd59746e0b79cea5f09eda9d55f1963d1de69cb
Maggie Leavell <mjleavell@gmail.com>
2018-09-07 12:06:44 -0500

    finished adding language objects

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const frenchDict = {
+    "hello": "bonjour", "goodbye": "au revoir", "please": "s'il vous plaît", "thank you": "merci beaucoup", "you're welcome": "de rien", "wine": "le vin", "bathroom": "toilette", "beer": "bière"
+};
+
+const germanDict = {
+    "hello": "hallo", "goodbye": "auf wiedersehen", "please": "bitte", "thank you": "danke schön", "you're welcome": "bitte", "wine": "wein", "bathroom": "toilette", "beer": "bier"
+};
+
+const spanishDict = {
+    "hello": "hola", "goodbye": "adiós", "please": "por favor", "thank you": "gracias", "you're welcome": "de nada", "wine": "vino", "bathroom": "baño", "beer": "cerveza"
+};
